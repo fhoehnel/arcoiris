ea40072aab357e893d3f7bb936ffe3ae93149224
Frank Hoehnel <frank_hoehnel@hotmail.com>
2020-01-17 21:32:18 +0100

    hotdeploy for Linux dev environment

--- src/main/java/de/webfilesys/ArcoirisBlog.java
+++ src/main/java/de/webfilesys/ArcoirisBlog.java
@@ -39,7 +39,7 @@ import de.webfilesys.util.CommonUtils;
 public class ArcoirisBlog {
     private static ArcoirisBlog instance = null;
 
-    public static final String VERSION = "Version 2.20.0 (08 Feb 2019)";
+    public static final String VERSION = "Version 2.21.0-beta1 (05 Jul 2019)";
 
     public static final String DEFAULT_MAIL_SENDER_ADDRESS = "arcoirisblog@nowhere.com";
 
