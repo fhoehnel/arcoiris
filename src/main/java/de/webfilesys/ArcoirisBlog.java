2c8668bdeff094186642cddca0efc637ee430315
Frank Höhnel <frank_hoehnel@hotmail.com>
2025-11-15 16:54:47 +0100

    new version number

--- src/main/java/de/webfilesys/ArcoirisBlog.java
+++ src/main/java/de/webfilesys/ArcoirisBlog.java
@@ -39,7 +39,7 @@ import de.webfilesys.util.CommonUtils;
 public class ArcoirisBlog {
     private static ArcoirisBlog instance = null;
 
-    public static final String VERSION = "Version 3.0.0-beta2 (23 Oct 2025)";
+    public static final String VERSION = "Version 3.1.0-beta1 (15 Nov 2025)";
 
     public static final String DEFAULT_MAIL_SENDER_ADDRESS = "arcoirisblog@nowhere.com";
 
